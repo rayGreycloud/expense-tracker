55e188fdb6abab25847b3b9c6fa113c1617c3618
rayGreycloud <raygreycloud@gmail.com>
2023-04-16 11:02:08 -0700

    add date util to check if recent

---
+++ utils/date.js
@@ -0,0 +1,7 @@
+export const isLast7Days = (dateString) => {
+  const inputDate = new Date(dateString);
+  const currentDate = new Date();
+  const diffTime = Math.abs(currentDate - inputDate);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays <= 7;
+};
