0a80248b72bd01966a8cf1a44bdae689e59b8199
rayGreycloud <raygreycloud@gmail.com>
2023-04-17 12:30:41 -0700

    refactor fetch methods - move try catch inside screen

--- utils/http.js
+++ utils/http.js
@@ -3,62 +3,46 @@ import axios from 'axios';
 import { DATEBASE_URL } from '../constants/databaseUrl';
 
 export const storeExpense = async (expenseData) => {
-  try {
-    const response = await axios.post(
-      `${DATEBASE_URL}/expenses.json`,
-      expenseData
-    );
+  const response = await axios.post(
+    `${DATEBASE_URL}/expenses.json`,
+    expenseData
+  );
 
-    return response.data.name;
-  } catch (error) {
-    console.log('storeExpense error: ', error);
-    return null;
-  }
+  return response.data.name;
 };
 
 export const fetchExpenses = async () => {
-  try {
-    const response = await axios.get(`${DATEBASE_URL}/expenses.json`);
-
-    const expenses = [];
-
-    for (const key in response.data) {
-      expenses.push({
-        id: key,
-        amount: response.data[key].amount,
-        date: new Date(response.data[key].date),
-        description: response.data[key].description
-      });
-    }
-
-    return expenses;
-  } catch (error) {
-    console.log('fetchExpenses error: ', error);
-    return null;
+  const response = await axios.get(`${DATEBASE_URL}/expenses.json`);
+  console.log('response.data: ', response.data);
+  if (!response.data) {
+    throw new Error('No data found');
   }
+  const expenses = [];
+
+  for (const key in response.data) {
+    expenses.push({
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description
+    });
+  }
+
+  return expenses;
 };
 
 export const updateExpense = async (id, expenseData) => {
-  try {
-    const response = await axios.put(
-      `${DATEBASE_URL}/expenses/${id}.json`,
-      expenseData
-    );
+  const response = await axios.put(
+    `${DATEBASE_URL}/expenses/${id}.json`,
+    expenseData
+  );
 
-    return response;
-  } catch (error) {
-    console.log('updateExpense error: ', error);
-    return null;
-  }
+  return response;
 };
 
 export const deleteExpense = async (id) => {
-  try {
-    const response = await axios.delete(`${DATEBASE_URL}/expenses/${id}.json`);
+  const response = await axios.delete(`${DATEBASE_URL}/expenses/${id}.json`);
+  console.log('response: ', response);
 
-    return response;
-  } catch (error) {
-    console.log('deleteExpense error: ', error);
-    return null;
-  }
+  return response;
 };
